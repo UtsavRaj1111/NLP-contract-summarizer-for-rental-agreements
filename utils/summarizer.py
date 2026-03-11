5539d379dd474706906a74257596e5634b0c75ef
Utsav Raj <utsavrajpratik@gmail.com>
2026-03-12 01:39:25 +0530

    Fix transformer summarization without pipeline

--- utils/summarizer.py
+++ utils/summarizer.py
@@ -1,25 +1,28 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import streamlit as st
-from transformers import pipeline
 
 @st.cache_resource
 def load_model():
-    return pipeline(
-        "summarization",
-        model="sshleifer/distilbart-cnn-12-6",
-        framework="pt"  
-    )
+    model_name = "sshleifer/distilbart-cnn-12-6"
 
-summarizer = load_model()
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+    return tokenizer, model
+
 
 def generate_summary(text):
-    if len(text) > 1000:
-        text = text[:1000]
+    tokenizer, model = load_model()
 
-    summary = summarizer(
-        text,
-        max_length=180,
-        min_length=80,
-        do_sample=False
+    inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
+
+    summary_ids = model.generate(
+        inputs["input_ids"],
+        max_length=120,
+        min_length=30,
+        length_penalty=2.0,
+        num_beams=4,
+        early_stopping=True,
     )
 
-    return summary[0]["summary_text"]
+    return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
